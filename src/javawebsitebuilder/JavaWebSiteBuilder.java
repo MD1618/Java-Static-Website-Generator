8472a59b5553d16693f379c6105af63a2b5e43b5
Martin Drahony <1636326@dundeeandangus.ac.uk>
2019-06-03 13:27:17 +0100

    First Commit

---
+++ src/javawebsitebuilder/JavaWebSiteBuilder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javawebsitebuilder;
+
+import java.util.ArrayList;
+
+/**
+ *Main Class. Instantiates the menu class.
+ * @author 1636326
+ */
+public class JavaWebSiteBuilder {
+
+    public static void main(String[] args) {
+
+        Menu menu1 = Menu.getInstance();
+        menu1.showMenu();
+
+    }
+
+}
